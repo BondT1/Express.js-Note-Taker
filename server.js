7c6fb293c211f29da4878d6bcbea2ab6948a84f4
BondT1 <tombond248@gmail.com>
2023-06-25 17:23:01 +0100

    server.js middleware and GET Routes

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express()
+const PORT = process.env.PORT || 3001;
+const path = require('path');
+const api = require('./routes/index.js');
+const data = require('./db/db.json');
+const fs = require('fs');
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`API Server situated on PORT: ${PORT}`)
+})
+
