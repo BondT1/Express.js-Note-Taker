0a76b6f17fbedd4ff44ee8c728767935f568b1a0
BondT1 <tombond248@gmail.com>
2023-06-26 15:00:38 +0100

    endless bug fixes

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index'));
 })
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/notes'))
 })
 
 app.listen(PORT, () => {
