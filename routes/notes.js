76ce6f2ab5c8193909b61c6fda8f6ee413522fcc
BondT1 <tombond248@gmail.com>
2023-06-29 18:05:06 +0100

    final changes

--- routes/notes.js
+++ routes/notes.js
@@ -13,7 +13,7 @@ notes.get('/:note_id', (req, res) => {
     readFromFile('./db/db.json')
         .then((data) => JSON.parse(data))
         .then((json) => {
-            const result = json.filter((note) => note.tip_id === tipId);
+            const result = json.filter((note) => note.note_id === noteId);
             return result.length > 0
                 ? res.json(result)
                 : res.json('No note found with that ID');
